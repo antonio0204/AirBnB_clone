90b5b1471ec7128ce545d16579235ad625b5421b
ronald0204 <raguirrevil0204@gmail.com>
2020-10-30 04:07:34 +0000

    from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,20 @@ import json
 class BaseModel:
     """BaseModel class
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel class objects.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+        else:
+            for key in kwargs:
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        self.__dict__[key] = datetime.datetime.strptime(kwargs[key],
+                                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                    else:
+                        self.__dict__[key] = kwargs[key]
 
     def save(self):
         """Update the public instance attribute 'update_at' with the current
