7bc93a3c2937164ff2f16e26eb40b31067596ccd
ronald0204 <raguirrevil0204@gmail.com>
2020-11-01 21:53:38 +0000

    Manage info about the amenity

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Define Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that enherits from BaseModel"""
+    name = ''
