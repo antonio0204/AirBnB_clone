15042cfa39c1a97b19cc65493f60ada84ae67e5e
aarizat <aarizatr@gmail.com>
2020-11-01 19:10:38 +0000

    Store objects

---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Define  FileStorage class
+"""
+from models.base_model import BaseModel
+from models.user import User
+import json
+import os
+
+
+class FileStorage:
+    """FileStorage class
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary.
+        """
+        return type(self).__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id
+
+        Args:
+            obj (BaseModel): BaseModel to set in __objects.
+        """
+        key = type(obj).__name__ + '.' + obj.id
+        type(self).__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file.
+        """
+        dict_objs = {obj_id: obj.to_dict()
+                     for obj_id, obj in type(self).__objects.items()}
+        with open(type(self).__file_path, mode='w') as file:
+            json.dump(dict_objs, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects.
+        """
+        if os.path.isfile(type(self).__file_path):
+            with open(type(self).__file_path, mode='r') as fp:
+                dict_of_dicts = json.load(fp)
+                for dict in dict_of_dicts.values():
+                    cls_name = dict['__class__']
+                    self.new(eval(cls_name)(**dict))
