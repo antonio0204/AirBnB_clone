6724568223c7920eb61ba282d6f0102be09ab695
ronald0204 <raguirrevil0204@gmail.com>
2020-11-01 22:33:38 +0000

    Update permissions

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Define City class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that enherits from BaseModel"""
+    state_id = ''
+    name = ''
